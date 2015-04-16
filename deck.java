5b2e9fa31b75420af33adbb64f251caea2a219a7
xSIXx <xSIXx@users.noreply.github.com>
2015-04-16 10:05:05 -0500

    Update deck.java

--- deck.java
+++ deck.java
@@ -1,17 +1,30 @@
 public class Deck{
   public Deck(String[] r, String[] s, int[] vals){ //ranks, suits, and values
-    for(int i = 0; i < s.length; i++){
+  
+    for(int i = 0; i < s.length; i++){ // get suits
       for(int x = 0; x < r.length; x++){
-        List getCard = new ArrayList(); // create card
-        getCard.add(r[i]);
+        List getCard = new ArrayList(); // create cards
+        getCard.add(r[x]);
         getCard.add(s[i])
-        getCard.add(vals[i])
+        getCard.add(vals[x])
        	
         cards.add(card);
+        
+        deckSize++;
       }
     }
+    
+    cardsLeft = deckSize;
+  }
+  
+  public boolean isEmpty{
+    return cardsLeft <= 0;
+  }
+  public int size{
+    return cardsLeft;
   }
   
   List cards = new ArrayList();
-  int size = 0;
+  int deckSize = 0;
+  int cardsLeft = 0;
 }
