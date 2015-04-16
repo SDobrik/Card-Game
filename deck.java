4ba6389546553123227b4316d2f16bbc3b5f726f
xSIXx <xSIXx@users.noreply.github.com>
2015-04-16 09:58:22 -0500

    Update deck.java

--- deck.java
+++ deck.java
@@ -1,1 +1,17 @@
-public class Deck
+public class Deck{
+  public Deck(String[] r, String[] s, int[] vals){ //ranks, suits, and values
+    for(int i = 0; i < s.length; i++){
+      for(int x = 0; x < r.length; x++){
+        List getCard = new ArrayList(); // create card
+        getCard.add(r[i]);
+        getCard.add(s[i])
+        getCard.add(vals[i])
+       	
+        cards.add(card);
+      }
+    }
+  }
+  
+  List cards = new ArrayList();
+  int size = 0;
+}
