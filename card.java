339a11d459f6b9b58ccdd886f2c99211bca9d6fa
SDobrik <dobrik.justskills@gmail.com>
2015-04-16 09:55:41 -0500

    Update card.java

--- card.java
+++ card.java
@@ -1,6 +1,25 @@
 public class card{
-  public void card(String rank, String suit, int pointValue){
+  //public state variables
+  private String rnk, st;
+  private int ptValue;
   
+  //
+  public card(String rank, String suit, int pointValue){
+  rnk= rank;
+  st=suit;
+  ptValue=pointValue;
   }
+  //accessor clases
+  public String rank(){
+  return rnk;
+  }
+  public String suit(){
+  return st;
+  }
+  public int pointValue(){
+  return ptValue;
+  }
+  
 
+  }
 }
