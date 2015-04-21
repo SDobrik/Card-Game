574b6d41c1016189bb2b921e379b6e449dd2749f
SDobrik <dobrik.justskills@gmail.com>
2015-04-21 08:30:07 -0500

    Update card.java

--- card.java
+++ card.java
@@ -1,33 +1,33 @@
 public class card{
-  //public state variables
-  private String rnk, st;
-  private int ptValue;
+    //public state variables
+    private String rnk, st;
+    private int ptValue;
   
-  //
-  public card(String rank, String suit, int pointValue){
-  rnk= rank;
-  st=suit;
-  ptValue=pointValue;
-  }
+     //
+    public card(String rank, String suit, int pointValue){
+    rnk= rank;
+    st=suit;
+    ptValue=pointValue;
+   }
   //accessor clases
-  public String rank(){
-  return rnk;
-  }
-  public String suit(){
-  return st;
-  }
-  public int pointValue(){
-  return ptValue;
-  }
+    public String rank(){
+      return rnk;
+    }
+    public String suit(){
+      return st;
+    }
+    public int pointValue(){
+      return ptValue;
+    }
   //Equality method
-  public boolean equalsTo(,,){
+    public boolean equalsTo(,,){
     
-  }
-  }
-  //to String
-  public String toString(){
-    return this.rnk +" of "+this.st+" (point value = "+this.ptValue+")";
-  }
+    }
+    
+    //to String
+    public String toString(){
+      return this.rnk +" of "+this.st+" (point value = "+this.ptValue+")";
+    }
   
   }
 }
