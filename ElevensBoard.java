d7c125e6b010829eb009563b64e62ece0be3d150
SDobrik <dobrik.justskills@gmail.com>
2015-04-22 11:12:54 -0500

    Update ElevensBoard.java

--- ElevensBoard.java
+++ ElevensBoard.java
@@ -1,9 +1,26 @@
 public class ElevensBoard {
 	public static void main(String[] args) {
-		Deck elevenGameCards = new Deck();
-		elevenGameCards.Shuffle();
-
+		Deck elevenGameCards = new Deck;
 		
+
+		// gui implementaion here
+		boolean runTheGame = true;
+		While(runTheGame){
+			elevenGameCards.Shuffle();
+
+			While (cardsRemaining = true){
+			if (reset== true){
+				continue;
+			}
+			//input method in a type of variable
+
+			Method1(/* variable from input method */);
+			//method2 is called from method 1
+
+
+			}
+		}
+
 	}
 
 }
